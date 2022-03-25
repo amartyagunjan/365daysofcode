901294de8b49664b59edc300f87b834fda5da0cf
amartyagunjan <amartyagujan01@gmail.com>
2022-03-25 22:35:58 +0530

    updated

--- Web Development/02_Nodejs/06_path.js
+++ Web Development/02_Nodejs/06_path.js
@@ -8,7 +8,17 @@ let path = require("path");
 //     fs.writeFileSync(path.join(__dirname , "lectures_" , dirPathToMake, "readme.md"), `#readme for ${dirPathToMake}`);
 // }
 
-let ext = path.extname(path.join(__dirname, "05_abc.text"));
+let ext = path.extname(path.join(__dirname, "05_abc.text")); // return the extention name of the file.
 console.log(ext);
-let baseName = path.basename(path.join(__dirname, "05_abc.text"));
+let baseName = path.basename(path.join(__dirname, "05_abc.text")); // return the base file
 console.log(baseName);
+
+/*
+------------------------------------- LINKS TO FOLLOW ---------------------------------------------------
+
+This link has details of the commonly used function in Path Module
+
+https://www.javascripttutorial.net/nodejs-tutorial/nodejs-path-module/#:~:text=in%20Node.js-,Node.,paths%20in%20the%20file%20system.
+
+
+*/
